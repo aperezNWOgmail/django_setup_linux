fba385fd5d343bfb6262bdb71f6c8d513ca3270d
exam70486 <65937647+Exam70486@users.noreply.github.com>
2025-10-07 03:45:19 +0000

    codesandbox updte

--- hello/urls.py
+++ hello/urls.py
@@ -3,4 +3,8 @@ from hello import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-]   
+    path("my_view", views.my_view, name="my_view"),
+    path('getAllLogs', views.getAllLogs,    name='getAllLogs'),
+    path('getAllPersons', views.getAllPersons, name='getAllPersons'),
+    path('getAllContactForms', views.getAllContactForms, name='getAllContactForms'),
+]
--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,124 @@
+from django.shortcuts import render
 from django.http import HttpResponse
-from hello.models import LogModel
+from django.db import connection
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import serializers
 
 
 def home(request):
+    return HttpResponse("Hello, Django!")
 
-    # Fetch all records
-    # records = YourModel.objects.all()
-    # Fetch a single record by id
-    # record = YourModel.objects.get(id=1)
-    # Filter records
-    # filtered_records = YourModel.objects.filter(field_name='value')
 
-    return HttpResponse("Hello, Python / Django!")
+def my_view(request):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT TOP 10 * FROM accessLogs order by ID_COLUMN DESC")
+        rows = cursor.fetchall()
+
+    # Process rows or pass them to the template context
+    context = {'rows': rows}
+    return render(request, 'my_template.html', context)
+
+# Define a serializer to handle the raw data.
+
+
+class RawDataSerializer(serializers.Serializer):
+    def to_representation(self, instance):
+        # Convert row tuple to dictionary
+        fields = [field[0] for field in self.context['cursor'].description]
+        return dict(zip(fields, instance))
+
+
+@api_view(['GET'])
+def getAllLogs(request):
+    try:
+        with connection.cursor() as cursor:
+            # Your raw SQL query
+            sql = """
+
+                 SELECT TOP 100
+                       AL.[ID_column]  id_Column
+                      ,AL.[PageName]   pageName
+                      ,AL.[AccessDate] accessDate
+                      ,AL.[IpValue]    ipValue
+                 FROM 
+                       [dbo].[accessLogs] AL
+                    WHERE
+                       AL.[LogType] = 1
+                    AND
+                        (AL.PAGENAME LIKE '%DEMO%'
+                    and
+                        AL.PAGENAME LIKE '%PAGE%')
+                    AND
+                        AL.PAGENAME NOT LIKE '%ERROR%'
+                    AND 
+                        AL.PAGENAME  NOT LIKE '%PAGE_DEMO_INDEX%'
+                    AND 
+                        UPPER(AL.PAGENAME) NOT LIKE '%CACHE%'
+                    AND
+                        AL.IPVALUE <> '::1'
+                 order by 
+                       AL.[ID_column] desc
+                  """
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            serializer = RawDataSerializer(rows, many=True, context={
+                                           'cursor': cursor})  # Pass cursor for field names
+            return Response(serializer.data)
+    except Exception as e:
+        return Response({'error': str(e)}, status=500)
+
+
+@api_view(['GET'])
+def getAllPersons(request):
+    try:
+        with connection.cursor() as cursor:
+            # Your raw SQL query
+            sql = """
+
+                  SELECT 
+                     [Id_Column]        id_Column
+                    ,[NombreCompleto]   nombreCompleto
+                    ,[ProfesionOficio]  profesionOficio
+                    ,[Ciudad]           ciudad
+                FROM
+                    [dbo].[Persona]
+                ORDER BY 
+                    Id_Column 
+
+                """
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            serializer = RawDataSerializer(rows, many=True, context={
+                                           'cursor': cursor})  # Pass cursor for field names
+            return Response(serializer.data)
+    except Exception as e:
+        return Response({'error': str(e)}, status=500)
+
+
+@api_view(['GET'])
+def getAllContactForms(request):
+    try:
+        with connection.cursor() as cursor:
+            # Your raw SQL query
+            sql = """
+
+                  SELECT 
+                        id         id 
+                        ,Name      name
+                        ,Email     field_1
+                        ,Message   field_2
+                        ,CreatedAt field_3
+                FROM
+                    ContactForm
+                ORDER BY 
+                    id desc
+                """
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            serializer = RawDataSerializer(rows, many=True, context={
+                                           'cursor': cursor})  # Pass cursor for field names
+            return Response(serializer.data)
+    except Exception as e:
+        return Response({'error': str(e)}, status=500)
