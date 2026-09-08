cef89e8e08ffc351952bb9ca306767260d02ecd4
Pablo Alejandro Perez Acosta <57961103+aperezNWO@users.noreply.github.com>
2026-09-08 06:08:55 +0000

    codespaces ubuntu update

--- web_project/settings.py
+++ web_project/settings.py
@@ -38,9 +38,16 @@ SECRET_KEY = 'django-insecure-v84z6@^x*2c^)*(57w*q8vi!0vl9oj$cje^9e5uvsa-wk2)d*o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['6rtfk8-8000.csb.app', 'localhost', 'http://localhost:4200',
-                 '127.0.0.1', 'https://apereznwo.github.io/','django-setup-linux.onrender.com','django-setup-linux-latest.onrender.com']
-
+ALLOWED_HOSTS = [   
+                    '6rtfk8-8000.csb.app'
+                    , 'localhost'
+                    , 'http://localhost:4200'
+                    , '127.0.0.1'
+                    , 'https://apereznwo.github.io/'
+                    , 'django-setup-linux.onrender.com'
+                    , 'django-setup-linux-latest.onrender.com'
+                    , 'django-setup-linux-latest-aqt9.onrender.com'
+                ]
 
 # Application definition
 
